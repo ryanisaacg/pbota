0b800265e4d9f2e7e7f0a0142e7de67daf69f59b
Ryan Goldstein <ryan@ryanisaacg.com>
2022-01-02 16:03:35 -0500

    You can now roll 2d6 via Discord. innovation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
-fn main() {
-    println!("Hello, world!");
+use fastrand::i32;
+use serenity::async_trait;
+use serenity::client::{Client, Context, EventHandler};
+use serenity::model::channel::Message;
+use serenity::framework::standard::{
+    StandardFramework,
+    CommandResult,
+    macros::{
+        command,
+        group
+    }
+};
+
+use std::env;
+
+#[group]
+#[commands(roll)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .group(&GENERAL_GROUP);
+
+    // Login with a bot token from the environment
+    let token = env::var("DISCORD_TOKEN").expect("token");
+    println!("{}", token);
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn roll(ctx: &Context, msg: &Message) -> CommandResult {
+    let d1 = i32(1..6);
+    let d2 = i32(1..6);
+    let message = format!("{}+{}={}", d1, d2, d1+d2);
+    msg.reply(ctx, message).await?;
+
+    Ok(())
 }

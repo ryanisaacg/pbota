397abda1b1385501af8f35487923e64ec5119421
Ryan Goldstein <ryan@ryanisaacg.com>
2022-01-02 17:40:46 -0500

    Changed the ~ to -

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ impl EventHandler for Handler {}
 #[tokio::main]
 async fn main() {
     let framework = StandardFramework::new()
-        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .configure(|c| c.prefix("-"))
         .group(&GENERAL_GROUP);
 
     // Login with a bot token from the environment
